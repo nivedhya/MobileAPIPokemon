0992331a2c9e9d6dcd9da5d2ca38cf4efc6d3e66
linto <lintov7@gmail.com>
2022-05-22 15:34:23 -0400

    Added code to fetch details from api

--- script.js
+++ script.js
@@ -1,118 +1,140 @@
+var selectedPokemon;
+var pokemons = [];
 
-document.querySelector("#search").addEventListener("click", getPokemon);
+async function getPokemonList() {
+  var response = await fetch(`https://pokeapi.co/api/v2/pokemon`);
+  if (response.ok) {
+    let json = await response.json();
+    if (json.results.length >= 5) {
+      pokemons = json.results.splice(0, 5)
+      await populatePokemonDetails();
+      showPokemons();
+    }
+
+  } else {
+    alert("HTTP Error");
+  }
+}
+
+
+async function populatePokemonDetails() {
+  for (var i = 0; i < pokemons.length; i++) {
+    var pokemon = pokemons[i];
+    pokemon.details = await getPokemonDetail(pokemon.name);
+  }
+}
+
+
+async function getPokemonDetail(name) {
+  var response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  if (response.ok) {
+    return await response.json();
+  } else {
+    console.log("Http error in getPokemonDetail");
+  }
+  return null;
+}
+
+
+async function showPokemons() {
+  var items = '';
+  pokemons.forEach((pokemon, index) => {
+    items = items + `
+    <div class="pokemon-item"  onClick="onPokemonClick(this, ${index})">
+      <img class="pokemon-img"
+        src="${pokemon.details.sprites.other["official-artwork"].front_default}"
+        alt="Pokemon name"
+      />
+    <div class="pokemon-name">
+      ${capFirstLetter(pokemon.details.name)}
+    </div>
+    </div>
+    `
+
+  });
+  document.querySelector("#pokemon-list").innerHTML = items;
+
+}
+function onPokemonClick(element, index) {
+  var activeClasses = document.querySelector(".pokemon-active");
+  if (activeClasses != null) {
+    activeClasses.classList.remove('pokemon-active');
+  }
+  element.classList.add('pokemon-active')
+  var pokemon = pokemons[index];
+  selectedPokemon = pokemon;
+  document.querySelector("#pokemon-specs").innerHTML = `
+  <h2>Specs</h2>
+  <div><b>Name: </b></span><span  class="answer">${pokemon.details.name}</div>
+  <div><b>Abilities: </b></span><span  class="answer">${getAbilityHtmlText(pokemon.details.abilities)}</div>
+  <div><b>Height: </b></span><spanclass="answer">${pokemon.details.height}</div>
+  <div><b>Base Experience: </b></span><span class="answer">${pokemon.details.base_experience}</div>`;
+  document.querySelector("#pokemon-abilities").innerHTML = ``;
+}
+
+
+function getAbilityHtmlText(abilities) {
+  var text = '';
+  abilities.forEach((ability, index) => {
+    if (text != '') {
+      text += ", ";
+    }
+    text = text + `<span class="pointer" onClick="getPokemonAbilityDetail('${ability.ability.url}')">${ability.ability.name}</span>`
+  });
+  return text;
+}
+
+function onAbilityClicked(index) {
+  if (selectedPokemon != null) {
+    alert(selectedPokemon.details.abilities[index].ability.name);
+  }
+}
+
+
+async function getPokemonAbilityDetail(url) {
+  var response = await fetch(url);
+  if (response.ok) {
+    var json = await response.json();
+    console.log(json)
+    var abilityEffect = "";
+    var shortEffect = "";
+    var flavorText = "";
+    var effectEntry = getEnglishLanguageItem(json.effect_entries)
+    if(effectEntry != null){
+      abilityEffect = effectEntry.effect
+      shortEffect = effectEntry.short_effect
+    }
+
+    var flavorEntry = getEnglishLanguageItem(json.flavor_text_entries)
+    if (flavorEntry != null) {
+      flavorText = flavorEntry.flavor_text;
+    }
+
+    document.querySelector("#pokemon-abilities").innerHTML = `
+      <h2>Abilities</h2>
+      <div><b>Ability Name: </b></span><span class="answer">${json.name}</div>
+      <div><b>Ability Effect: </b></span><span class="answer">${abilityEffect}</div>
+      <div><b>Short Effect: </b></span><span class="answer">${shortEffect}</div>
+      <div><b>Flavour Text: </b></span><span class="answer">${flavorText}</div>`
+  }
+}
+
+function getEnglishLanguageItem(list){
+  if (list.length > 0) {
+    for(var i =0;i<list.length; i++){
+      var item = list[i]
+      if(item.language != null && item.language.name == "en"){
+        return item;
+      }
+    }
+  }
+}
+
+
 
 
 function capFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function lowCaseName(string) {
-  return string.toLowerCase();
-}
-
-
-
-
-function getPokemon() {
-
-
-  fetch(`https://pokeapi.co/api/v2/pokemon/ditto`)
-    .then((response) => response.json())
-    .then((data) => {
-      document.querySelector(".pokemonimg").innerHTML = `
-      <div>
-        <img
-          src="${data.sprites.other["official-artwork"].front_default}"
-          alt="Pokemon name"
-        />
-      </div>
-      <div class="PInfo">
-        <h1>${capFirstLetter(data.name)}</h3>
-        <p>Weight: ${data.weight}</p>
-      </div>`;
-
-    })
-
-
-    fetch(`https://pokeapi.co/api/v2/pokemon/bulbasaur`)
-    .then((response) => response.json())
-    .then((data) => {
-      document.querySelector(".pokemonimg1").innerHTML = `
-      <div>
-        <img
-          src="${data.sprites.other["official-artwork"].front_default}"
-          alt="Pokemon bulbasaur"
-        />
-      </div>    
-      <div class="PInfo">
-        <h1>${capFirstLetter(data.name)}</h3>
-        <p>Weight: ${data.weight}</p>
-      </div>`;
-
-    })
-
-
-
-    fetch(`https://pokeapi.co/api/v2/pokemon/ivysaur`)
-    .then((response) => response.json())
-    .then((data) => {
-      document.querySelector(".pokemonimg2").innerHTML = `
-      <div>
-        <img
-          src="${data.sprites.other["official-artwork"].front_default}"
-          alt="Pokemon bulbasaur"
-        />
-      </div>    
-      <div class="PInfo">
-        <h1>${capFirstLetter(data.name)}</h3>
-        <p>Weight: ${data.weight}</p>
-      </div>`;
-
-    })
-
-
-    fetch(`https://pokeapi.co/api/v2/pokemon/charmander`)
-    .then((response) => response.json())
-    .then((data) => {
-      document.querySelector(".pokemonimg3").innerHTML = `
-      <div>
-        <img
-          src="${data.sprites.other["official-artwork"].front_default}"
-          alt="Pokemon bulbasaur"
-        />
-      </div>    
-      <div class="PInfo">
-        <h1>${capFirstLetter(data.name)}</h3>
-        <p>Weight: ${data.weight}</p>
-      </div>`;
-
-    })
-
-
-
-
-    fetch(`https://pokeapi.co/api/v2/pokemon/charizard`)
-    .then((response) => response.json())
-    .then((data) => {
-      document.querySelector(".pokemonimg4").innerHTML = `
-      <div>
-        <img
-          src="${data.sprites.other["official-artwork"].front_default}"
-          alt="Pokemon bulbasaur"
-        />
-      </div>    
-      <div class="PInfo">
-        <h1>${capFirstLetter(data.name)}</h3>
-        <p>Weight: ${data.weight}</p>
-      </div>`;
-
-    })
-    .catch((err) => {
-      document.querySelector(".pokemonBox").innerHTML = `
-      <h4>Pokemon not found 😞</h4>
-      `;
-      console.log("Pokemon not found", err);
-    });
-
-  e.preventDefault();
-}
+window.onload = getPokemonList;
